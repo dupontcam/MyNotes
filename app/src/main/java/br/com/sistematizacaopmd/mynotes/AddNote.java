95042968b203c5f7bf83129d77314ddf50df0d34
dupontcam <125046483+dupontcam@users.noreply.github.com>
2024-10-04 10:39:56 -0300

    Add Date and Time

--- app/src/main/java/br/com/sistematizacaopmd/mynotes/AddNote.java
+++ app/src/main/java/br/com/sistematizacaopmd/mynotes/AddNote.java
@@ -1,6 +1,9 @@
 package br.com.sistematizacaopmd.mynotes;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -10,10 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class AddNote extends AppCompatActivity {
 
     Toolbar toolbar;
     EditText noteTitle, noteDetails;
+    Calendar c;
+    String todaysDate, currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +29,47 @@ public class AddNote extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("New Note");
 
         noteTitle = findViewById(R.id.noteTitle);
         noteDetails = findViewById(R.id.noteDetails);
+
+        noteTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() != 0) {
+                    getSupportActionBar().setTitle(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // get current date and time
+        c = Calendar.getInstance();
+        todaysDate = c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH) + 1) + "/" + c.get(Calendar.YEAR);
+        currentTime = pad(c.get(Calendar.HOUR)) + ":" + pad(c.get(Calendar.MINUTE));
+
+        Log.d("calendar", "Date and Time: " + todaysDate + " " + currentTime);
+    }
+
+    private String pad(int i) {
+        if(i < 10)
+            return "0" + i;
+        return String.valueOf(i);
     }
 }
--- app/src/main/java/br/com/sistematizacaopmd/mynotes/MainActivity.java
+++ app/src/main/java/br/com/sistematizacaopmd/mynotes/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.sistematizacaopmd.mynotes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -30,5 +35,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add) {
+            startActivity(new Intent(this, AddNote.class));
+            Toast.makeText(this, "Add btn is Clicked.", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
